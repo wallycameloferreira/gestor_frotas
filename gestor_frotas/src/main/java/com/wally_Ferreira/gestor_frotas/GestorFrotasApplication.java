95df2a96843eed4663560c64e2dc111d05cdd64c
wally ferreira <wallycamelo@gmail.com>
2025-07-02 18:12:34 -0300

    Primeiro commit do projeto

---
+++ gestor_frotas/src/main/java/com/wally_Ferreira/gestor_frotas/GestorFrotasApplication.java
@@ -0,0 +1,14 @@
+package com.wally_Ferreira.gestor_frotas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorFrotasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorFrotasApplication.class, args);
+		System.out.println("Boa tarde");
+	}
+
+}
---
+++ gestor_frotas/src/test/java/com/wally_Ferreira/gestor_frotas/GestorFrotasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wally_Ferreira.gestor_frotas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorFrotasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
